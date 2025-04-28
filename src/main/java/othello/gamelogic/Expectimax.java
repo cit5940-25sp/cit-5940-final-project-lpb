933364a48325c3c8dde1b2c9f1f214456fcb7d75
riona <wriona@seas.upenn.edu>
2025-04-27 20:01:24 -0400

    for pulling

--- src/main/java/othello/gamelogic/Expectimax.java
+++
@@ -1,8 +0,0 @@
-package othello.gamelogic;
-
-public class Expectimax implements AI {
-    @Override
-    public BoardSpace nextMove(BoardSpace[][] board, Player player) {
-        return null;
-    }
-}
--- src/main/java/othello/gamelogic/Minimax.java
+++ src/main/java/othello/gamelogic/Minimax.java
@@ -7,8 +7,13 @@ public class Minimax implements AI {
     @Override
     public BoardSpace nextMove(BoardSpace[][] board, Player player) {
         Map<BoardSpace, List<BoardSpace>> moves = player.getAvailableMoves(board);
-        Deque<BoardSpace> queue = new ArrayDeque<>();
+        if (moves.isEmpty()) {
+            return null;
+        }
 
+        Deque<BoardSpace> queue = new ArrayDeque<>();
+        BoardSpace bestMove = null;
+        int bestScore = Integer.MIN_VALUE;
         // use BFS
         // reverse strategy at each level (minimize opponent gain/maximize self gain)
         return null;
